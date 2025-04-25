999ec078c466f7b5b2d38116626a47390e372916
Loïse Fenoll <admin@fingertips.fr>
2025-04-25 12:39:19 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,41 @@
+# Set the required provider and versions
+terraform {
+  required_providers {
+    scalingo = {
+      source  = "scalingo/scalingo"
+      version = "2.3.0" # Or the latest version
+    }
+  }
+}
+
+variable "scalingo_token" {
+  type        = string
+  description = "the scalingo token"
+}
+
+provider "scalingo" {
+  api_token = var.scalingo_token
+  region    = "osc-fr1"
+}
+
+resource "scalingo_app" "python-api" {
+  name     = "python-api"
+  environment = {
+    PROJECT_DIR="server",
+    BUILDPACK_URL = "https://github.com/Scalingo/python-buildpack"
+  }
+}
+
+resource "scalingo_addon" "db" {
+  provider_id = "mysql"
+  plan = "mysql-starter-512"
+  app = "${scalingo_app.python-api.id}"
+}
+
+resource "scalingo_container_type" "web" {
+  app    = scalingo_app.python-api.name
+  name   = "web"
+  amount = 1
+  size   = "S"
+}
+
---
+++ server/app.py
@@ -0,0 +1,37 @@
+import mysql.connector
+import os
+from flask import Flask
+from flask import render_template
+from urllib.parse import urlparse
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
+@app.route("/users")
+def get_users():
+    mysqlUrl = os.getenv("SCALINGO_MYSQL_URL")
+    dbc = urlparse(mysqlUrl)
+    conn = mysql.connector.connect(
+        database=dbc.path.lstrip('/'),
+        user=dbc.username,
+        password=dbc.password,
+        port=dbc.port, 
+        host=dbc.hostname
+    )
+    cursor = conn.cursor()
+    sql_select_Query = "select * from users"
+    cursor.execute(sql_select_Query)
+    # get all records
+    records = cursor.fetchall()
+    print("Total number of rows in table: ", cursor.rowcount)
+    # renvoyer nos données et 200 code OK
+    return {'users': records}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+    
